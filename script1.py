49681a02a1eb8be528b3f6a9462b1c0d0491e544
ukates <derrickcates@outlook.com>
2019-10-12 18:12:39 -0400

    Adding functions for editing and displaying

--- script1.py
+++ script1.py
@@ -1,10 +1,31 @@
 import sqlite3
-#create connection object
-conn = sqlite3.connect("lite.db")
-#create a cursor object
-cur = conn.cursor()
-#sql code
-cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
-cur.execute("INSERT INTO store VALUES ('Wine Glass', 8, 10.5)")
-conn.commit()
-conn.close()
+
+def create_table():
+    #create connection object
+    conn = sqlite3.connect("lite.db")
+    #create a cursor object
+    cur = conn.cursor()
+    #sql code
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+    conn.commit()
+    conn.close()
+
+def insert(item, quantity, price):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO store VALUES (?,?,?)", (item, quantity, price))
+    conn.commit()
+    conn.close()
+
+insert("Water Glass", 10, 5)
+insert("Coffee Cup", 7, 3.6)
+
+def view():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM store")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+print(view())
